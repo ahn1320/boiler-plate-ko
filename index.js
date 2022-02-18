8b35f7002146a6ed9cef262440d1f0bf9ca5c47a
ahn1320 <skyhot1320@gmail.com>
2022-02-18 06:40:32 -0800

    Update User schema and index and add bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jin:123@cluster0.rmpdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -8,6 +14,19 @@ mongoose.connect('mongodb+srv://jin:123@cluster0.rmpdv.mongodb.net/myFirstDataba
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {res.send('Hello World!~~안녕하세요 ~')})
+app.get('/', (req, res) => {res.send('Hello World!~~안녕하세요 ~ 새해복 많이 받으세요   ')})
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
