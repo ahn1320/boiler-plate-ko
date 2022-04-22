961e5d975d0e2ab2bc1cd7e026c1a800ff33bb4c
ahn1320 <skyhot1320@gmail.com>
2022-04-21 18:42:41 -0700

    Added auth

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
+
+export default function (SpecificComponent, option, adminRoute = null) {
+
+    // option:
+    // null => 아무나 출입이 가능한 페이지
+    // true => 로그인한 유저만 출입이 가능한 페이지
+    // false => 로그인한 유저는 출입 불가능한 페이지
+
+    function AuthenticationCheck(props) {
+        const dispatch = useDispatch();
+
+        useEffect(() => {
+            dispatch(auth()).then(response => {
+                console.log(response);
+
+
+                // 로그인 하지 않은 상태
+                if (!response.payload.isAuth) {
+                    if (option) {
+                        props.history.push('/login');
+                    }
+                } else {
+                    // 로그인 한 상태
+                    // 어드민은 아닌데 어드민만 들어갈 수 있는 페이지를 들어갈려고 할때
+                    if (adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/');
+                    } else {
+                        // 로그인한 유저가 출입불가능한 페이지를 갈려고 할때
+                        if (option === false)
+                            props.history.push('/');
+                    }
+                }
+            })
+        }, [])
+
+        return (
+            <SpecificComponent />
+        )
+    }
+
+
+    return AuthenticationCheck
+}
